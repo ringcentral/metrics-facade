0f540052fb0ebf4a333642c6e06538f699e3dbb4
devromik <dev.romik@gmail.com>
2023-05-19 13:56:59 +0400

    Update listings

--- metrics-facade-articles/src/main/java/com/ringcentral/platform/metrics/articles/intro/Listing04.java
+++ metrics-facade-articles/src/main/java/com/ringcentral/platform/metrics/articles/intro/Listing04.java
@@ -1,14 +1,15 @@
 package com.ringcentral.platform.metrics.articles.intro;
 
 import com.ringcentral.platform.metrics.defaultImpl.DefaultMetricRegistry;
-import com.ringcentral.platform.metrics.histogram.Histogram;
 import com.ringcentral.platform.metrics.histogram.Histogram.Bucket;
 import com.ringcentral.platform.metrics.reporters.prometheus.PrometheusMetricsExporter;
+import com.ringcentral.platform.metrics.timer.Timer;
 
 import static com.ringcentral.platform.metrics.counter.Counter.COUNT;
 import static com.ringcentral.platform.metrics.histogram.Histogram.TOTAL_SUM;
-import static com.ringcentral.platform.metrics.histogram.configs.builders.HistogramConfigBuilder.withHistogram;
 import static com.ringcentral.platform.metrics.names.MetricName.withName;
+import static com.ringcentral.platform.metrics.timer.configs.builders.TimerConfigBuilder.withTimer;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class Listing04 {
 
@@ -17,19 +18,19 @@ public class Listing04 {
         var registry = new DefaultMetricRegistry();
 
         // 2) Register metric.
-        Histogram histogram = registry.histogram(withName("request", "duration", "seconds"), () -> withHistogram()
+        Timer timer = registry.timer(withName("request", "duration", "seconds"), () -> withTimer()
             // 3) Specify custom set of measurables
             .measurables(
                 COUNT,
                 TOTAL_SUM,
-                Bucket.of(5),
-                Bucket.of(10),
-                Bucket.of(15)));
+                Bucket.of(5, SECONDS),
+                Bucket.of(10, SECONDS),
+                Bucket.of(15, SECONDS)));
 
         // 4) Let's assume that there were 3 requests: 5, 10 and 15 seconds long
-        histogram.update(5);
-        histogram.update(10);
-        histogram.update(15);
+        timer.update(5, SECONDS);
+        timer.update(10, SECONDS);
+        timer.update(15, SECONDS);
 
         // 5) Create exporter
         var exporter = new PrometheusMetricsExporter(registry);
